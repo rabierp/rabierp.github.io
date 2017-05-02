c38626b8db0fd5ecf248c82ea88704f779f52ed5
rabierp <pascal.rabier@gmail.com>
2017-05-03 00:48:14 +0200

    Update documentation

--- push2gh.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/bash
-pelican content -o output -s publishconf.py
-git push -u origin pelican
-ghp-import -b master output
-git push git@github.com:rabierp/rabierp.github.io.git master:master
